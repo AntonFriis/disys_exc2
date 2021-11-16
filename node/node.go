b4351126d204a1d73dc4622a8671413815031bc0
AntonFriis <anlf@itu.dk>
2021-11-16 13:07:22 +0100

    Created a Node which is both a Client and a server. But currently the nodes always connect on port 0000

--- node/node.go
+++ node/node.go
@@ -1,1 +1,151 @@
-package node
+package main
+
+import (
+	"bufio"
+	pb "disys_exc2/p2p"
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type node struct {
+	client pb.P2PServiceClient
+	server pb.UnimplementedP2PServiceServer
+	Addr string
+	Id int64
+	IsFirst bool
+}
+
+func (node node) mustEmbedUnimplementedP2PServiceServer() {
+	panic("implement me")
+}
+
+func (node node) Disconnect(ctx context.Context, send *pb.Send) (*pb.Response, error) {
+	return &pb.Response{Message: "Node "+ strconv.FormatInt(node.Id, 10) + " has disconnected!"}, nil
+}
+
+var conn *grpc.ClientConn
+var ctx context.Context
+var LastId int64
+
+func NewNode(Id int64) *node {
+	node := node{
+		client: nil,
+		Addr: Port(Id),
+		Id: Id,
+		IsFirst: true,
+	}
+	return &node
+}
+
+// Port TODO: Fix cursed code
+func Port(NodeId int64) string {
+	file, err := os.Open("ports.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	var Port0 string //Fix cursed line
+	for scanner.Scan() {
+		IdPort := strings.Split(scanner.Text(), " ")
+		Id, _ := strconv.ParseInt(IdPort[0], 10, 64)
+		if Id == 0 {Port0 = IdPort[1]} //Fix cursed line
+		if Id == NodeId {return IdPort[1]}
+	}
+	return Port0 //Fix cursed line
+}
+
+func (node *node) Connect(ctx context.Context, send *pb.Send) (*pb.Response, error) {
+	var err error
+	ConPort := Port(send.Port)
+	conn, err = grpc.Dial(ConPort, grpc.WithInsecure())
+	return &pb.Response{Message: "Node "+ strconv.FormatInt(node.Id, 10) + " has connected on port: " + ConPort}, err
+}
+
+func (node *node) listen() {
+	log.Printf("Node %v: listening on port: %v\n", node.Id, node.Addr)
+	lis, err := net.Listen("tcp", node.Addr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	server := grpc.NewServer() // n is for serving purpose
+
+	pb.RegisterP2PServiceServer(server, node.server)
+	// Register reflection service on gRPC server.
+	reflection.Register(server)
+
+	// start listening
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func main() {
+	// pass the port as an argument and also the port of the other node
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("Arguments required: <id> as int 0-4")
+		os.Exit(1)
+	}
+
+	// args in order
+	Id, err := strconv.ParseInt(args[0], 10, 64)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	//Setup the file for log outputs
+	LogFile := "./systemlogs/node.log"
+	// open log file
+	logFile, err := os.OpenFile(LogFile, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer func(logFile *os.File) {
+		err := logFile.Close()
+		if err != nil {
+			log.Fatalf("File not found: %v", err)
+		}
+	}(logFile)
+
+	log.SetOutput(logFile)
+
+	LastId := pb.LatestId{Id: LastId}
+
+	//Create node
+	node := NewNode(Id)
+
+	//Increment LastId by 1
+	LastId.Id = LastId.Id+1
+
+	//var opts []grpc.DialOption
+	// Set up a connection to the server.
+	msg, err := node.Connect(ctx, &pb.Send{Port: 0})
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	log.Printf(msg.Message)
+	defer conn.Close()
+
+	node.client = pb.NewP2PServiceClient(conn)
+
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Minute)
+	defer cancel()
+
+	go node.listen()
+
+	for {
+		time.Sleep(60 * time.Second)
+	}
+}
+
--- p2p/p2p_grpc.pb.go
+++ p2p/p2p_grpc.pb.go
@@ -18,122 +18,122 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// ChatServiceClient is the client API for ChatService service.
+// P2PServiceClient is the client API for P2PService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type ChatServiceClient interface {
+type P2PServiceClient interface {
 	Connect(ctx context.Context, in *Send, opts ...grpc.CallOption) (*Response, error)
 	Disconnect(ctx context.Context, in *Send, opts ...grpc.CallOption) (*Response, error)
 }
 
-type chatServiceClient struct {
+type p2PServiceClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewChatServiceClient(cc grpc.ClientConnInterface) ChatServiceClient {
-	return &chatServiceClient{cc}
+func NewP2PServiceClient(cc grpc.ClientConnInterface) P2PServiceClient {
+	return &p2PServiceClient{cc}
 }
 
-func (c *chatServiceClient) Connect(ctx context.Context, in *Send, opts ...grpc.CallOption) (*Response, error) {
+func (c *p2PServiceClient) Connect(ctx context.Context, in *Send, opts ...grpc.CallOption) (*Response, error) {
 	out := new(Response)
-	err := c.cc.Invoke(ctx, "/P2P.ChatService/Connect", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/p2p.p2pService/Connect", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *chatServiceClient) Disconnect(ctx context.Context, in *Send, opts ...grpc.CallOption) (*Response, error) {
+func (c *p2PServiceClient) Disconnect(ctx context.Context, in *Send, opts ...grpc.CallOption) (*Response, error) {
 	out := new(Response)
-	err := c.cc.Invoke(ctx, "/P2P.ChatService/Disconnect", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/p2p.p2pService/Disconnect", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// ChatServiceServer is the server API for ChatService service.
-// All implementations must embed UnimplementedChatServiceServer
+// P2PServiceServer is the server API for P2PService service.
+// All implementations must embed UnimplementedP2PServiceServer
 // for forward compatibility
-type ChatServiceServer interface {
+type P2PServiceServer interface {
 	Connect(context.Context, *Send) (*Response, error)
 	Disconnect(context.Context, *Send) (*Response, error)
-	mustEmbedUnimplementedChatServiceServer()
+	mustEmbedUnimplementedP2PServiceServer()
 }
 
-// UnimplementedChatServiceServer must be embedded to have forward compatible implementations.
-type UnimplementedChatServiceServer struct {
+// UnimplementedP2PServiceServer must be embedded to have forward compatible implementations.
+type UnimplementedP2PServiceServer struct {
 }
 
-func (UnimplementedChatServiceServer) Connect(context.Context, *Send) (*Response, error) {
+func (UnimplementedP2PServiceServer) Connect(context.Context, *Send) (*Response, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Connect not implemented")
 }
-func (UnimplementedChatServiceServer) Disconnect(context.Context, *Send) (*Response, error) {
+func (UnimplementedP2PServiceServer) Disconnect(context.Context, *Send) (*Response, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Disconnect not implemented")
 }
-func (UnimplementedChatServiceServer) mustEmbedUnimplementedChatServiceServer() {}
+func (UnimplementedP2PServiceServer) mustEmbedUnimplementedP2PServiceServer() {}
 
-// UnsafeChatServiceServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to ChatServiceServer will
+// UnsafeP2PServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to P2PServiceServer will
 // result in compilation errors.
-type UnsafeChatServiceServer interface {
-	mustEmbedUnimplementedChatServiceServer()
+type UnsafeP2PServiceServer interface {
+	mustEmbedUnimplementedP2PServiceServer()
 }
 
-func RegisterChatServiceServer(s grpc.ServiceRegistrar, srv ChatServiceServer) {
-	s.RegisterService(&ChatService_ServiceDesc, srv)
+func RegisterP2PServiceServer(s grpc.ServiceRegistrar, srv P2PServiceServer) {
+	s.RegisterService(&P2PService_ServiceDesc, srv)
 }
 
-func _ChatService_Connect_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _P2PService_Connect_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Send)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ChatServiceServer).Connect(ctx, in)
+		return srv.(P2PServiceServer).Connect(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/P2P.ChatService/Connect",
+		FullMethod: "/p2p.p2pService/Connect",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ChatServiceServer).Connect(ctx, req.(*Send))
+		return srv.(P2PServiceServer).Connect(ctx, req.(*Send))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _ChatService_Disconnect_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _P2PService_Disconnect_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Send)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ChatServiceServer).Disconnect(ctx, in)
+		return srv.(P2PServiceServer).Disconnect(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/P2P.ChatService/Disconnect",
+		FullMethod: "/p2p.p2pService/Disconnect",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ChatServiceServer).Disconnect(ctx, req.(*Send))
+		return srv.(P2PServiceServer).Disconnect(ctx, req.(*Send))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// ChatService_ServiceDesc is the grpc.ServiceDesc for ChatService service.
+// P2PService_ServiceDesc is the grpc.ServiceDesc for P2PService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var ChatService_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "P2P.ChatService",
-	HandlerType: (*ChatServiceServer)(nil),
+var P2PService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "p2p.p2pService",
+	HandlerType: (*P2PServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "Connect",
-			Handler:    _ChatService_Connect_Handler,
+			Handler:    _P2PService_Connect_Handler,
 		},
 		{
 			MethodName: "Disconnect",
-			Handler:    _ChatService_Disconnect_Handler,
+			Handler:    _P2PService_Disconnect_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
